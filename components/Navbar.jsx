de7299c43b14b42f888224bfb8bf91369f9cf773
Franco Carini <carini.franco@gmail.com>
2022-05-06 22:36:29 -0300

    Added Add Task Button HTML

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,15 @@
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material'
 import TodayIcon from '@mui/icons-material/Today'
 
 const Navbar = () => {
   return (
     <AppBar position="sticky" sx={{ backgroundColor: '#3f51b5' }}>
-      <Toolbar>
-        <TodayIcon />
-        <Typography variant="h6">NextJS Kanban</Typography>
+      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Box component="div" sx={{ display: 'flex', alignItems: 'center' }}>
+          <TodayIcon />
+          <Typography variant="h6">NextJS Kanban</Typography>
+        </Box>
+        <Button variant="contained">Add Task</Button>
       </Toolbar>
     </AppBar>
   )
